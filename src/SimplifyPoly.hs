e99ed166593b5183f56650ce2b17cef96ea65135
Amir Barylko <amir@barylko.com>
2016-10-05 20:53:28 -0700

    Implemented terms to parse sign, number and var

--- src/SimplifyPoly.hs
+++ src/SimplifyPoly.hs
@@ -2,15 +2,12 @@ module SimplifyPoly where
 
 import Data.List
 import Data.Maybe
-import Text.Regex
-import Debug.Trace
 import Text.Read
 
 simplify :: String -> String
-simplify = rmPlus . concatMap toTerm . solve . parseTerms
+simplify = rmPlus . concatMap toTerm . solve . terms
   where
     solve = sortBy shortest . map sumTerms . groupBy sameFst . sort 
-    parseTerms = map parse . terms
     shortest (a, _) (b, _) = compare (length a) (length b)
     sumTerms = foldl1 (\(v, n1) (_, n2) -> (v, n1 + n2))
     sameFst (a, _) (b, _) = a == b
@@ -23,15 +20,34 @@ simplify = rmPlus . concatMap toTerm . solve . parseTerms
 
 rmPlus = dropWhile ((==) '+')
 
-terms = fromMaybe [] . fmap mkTuple . matchRegexAll re
+terms = fromMaybe [] . fmap mkTuple . parseTerm
   where
-    re = mkRegex "([\\+-]?[0-9]*)([a-z]+)"
-    mkTuple (_, _, rest, [num, var]) = (var, num) : terms rest
+    mkTuple (rest, term) = term : terms rest
 
-parse (var, num) = ((sort var), (parse' num))
+parseTerm "" = Nothing
+parseTerm str = Just (rest2, (var, num))
   where
-    parse' ""  = 1
-    parse' "+" = 1
-    parse' "-" = -1
-    parse' num = read $ rmPlus num
-    rd = read :: String -> Int
+    (sg, rest) = parseSign str
+    (qt, rest1) = parseNum rest
+    (var, rest2) = parseVar rest1
+    num = sg * qt
+
+parseSign (a:xs)
+  | a == '+' = (1, xs)
+  | a == '-' = (-1, xs)
+  | otherwise = (1, a:xs)
+
+parseNum str = ((toNum digits), rest)
+  where
+    digits = takeWhile isDigit str
+    rest = drop (length digits) str
+    isDigit = flip elem ['0'..'9']
+    toNum "" = 1
+    toNum xs = read xs
+
+parseVar str = (var, rest)
+  where
+    isChar = flip elem ['a'..'z']
+    var = sort $ takeWhile isChar str
+    rest = drop (length var) str
+
