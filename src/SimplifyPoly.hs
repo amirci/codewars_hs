dd5c14f84cbdbfde20746a2a48f6a6af47690f9c
Amir Barylko <amir@barylko.com>
2016-10-06 20:28:52 -0700

    Refactored parseTerm, added `on` to compare, added isAsciiLower and isDigit, changed terms to unfoldr

--- src/SimplifyPoly.hs
+++ src/SimplifyPoly.hs
@@ -1,17 +1,19 @@
 module SimplifyPoly where
 
+import Data.Char (isAsciiLower, isDigit)
+import Data.Function (on)
 import Data.List
 import Data.Maybe
 import Text.Read
 
 simplify :: String -> String
-simplify = rmPlus . concatMap toTerm . solve . terms
+simplify = rmPlus . concatMap showTerm . solve . unfoldr parseTerm
   where
-    solve = sortBy shortest . map sumTerms . groupBy sameFst . sort 
-    shortest (a, _) (b, _) = compare (length a) (length b)
+    solve = sortBy shortest . map sumTerms . groupBy sameFst . sort
+    shortest = compare `on` (length . fst)
     sumTerms = foldl1 (\(v, n1) (_, n2) -> (v, n1 + n2))
-    sameFst (a, _) (b, _) = a == b
-    toTerm (var, n)
+    sameFst = (==) `on` fst
+    showTerm (var, n)
       | n == 1 = '+' : var
       | n >  1 = '+' : show n ++ var
       | n == 0 = ""
@@ -20,28 +22,18 @@ simplify = rmPlus . concatMap toTerm . solve . terms
 
 rmPlus = dropWhile ((==) '+')
 
-terms = fromMaybe [] . fmap mkTuple . parseTerm
-  where
-    mkTuple (rest, term) = term : terms rest
-
 parseTerm "" = Nothing
-parseTerm str = Just (rest2, (var, num))
+parseTerm str = Just ((var, sg * qt), rest2)
   where
-    (sg, rest) = parseSign str
-    (qt, rest1) = mapFst toNum $ splitWhile isDigit rest
-    (var, rest2) = mapFst sort $ splitWhile isVar rest1
-    num = sg * qt
-    isDigit = flip elem ['0'..'9']
-    isVar = flip elem ['a'..'z']
-    toNum "" = 1
-    toNum xs = read xs
+    (sg, rest)   = mapFst toSign $ splitWhile isSign str
+    (qt, rest1)  = mapFst toNum  $ splitWhile isDigit rest
+    (var, rest2) = mapFst sort   $ splitWhile isAsciiLower rest1
+    isSign = flip elem ['+', '-']
+    toNum = fromMaybe 1 . readMaybe
+    toSign "-" = -1
+    toSign xs  = 1
 
 mapFst fn (a, b) = ((fn a), b)
-
-parseSign (a:xs)
-  | a == '+' = (1, xs)
-  | a == '-' = (-1, xs)
-  | otherwise = (1, a:xs)
 
 splitWhile pred str = (consumed, rest)
   where
